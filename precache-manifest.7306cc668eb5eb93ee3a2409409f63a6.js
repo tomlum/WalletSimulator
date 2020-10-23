66323c716ac3f7a4ec763addafd9f00b66e740b9
tomlum <tomlumperson@gmail.com>
2020-10-23 19:30:12 -0400

    Updates

---
+++ precache-manifest.7306cc668eb5eb93ee3a2409409f63a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6327068c69e001d7936f8d58dc271e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191e3cd87967704b3345",
+    "url": "/static/css/main.cf02fb6c.chunk.css"
+  },
+  {
+    "revision": "f464ddff720dbb4f3c92",
+    "url": "/static/js/2.ddbfc22c.chunk.js"
+  },
+  {
+    "revision": "ab4368d17c9657deaf626b9459f8b13a",
+    "url": "/static/js/2.ddbfc22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191e3cd87967704b3345",
+    "url": "/static/js/main.6873aa0f.chunk.js"
+  },
+  {
+    "revision": "47c1e5653729d5961aee",
+    "url": "/static/js/runtime-main.738ddf30.js"
+  }
+]);
